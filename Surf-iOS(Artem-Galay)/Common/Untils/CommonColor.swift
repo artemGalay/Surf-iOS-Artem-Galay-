b85a180bc5888a92044d73a16c6e749aff829d34
Artem Galay <iostema@mail.ru>
2023-02-03 10:50:45 +0300

    Add colors.

---
+++ Surf-iOS(Artem-Galay)/Common/Untils/CommonColor.swift
@@ -0,0 +1,15 @@
+//
+//  CommonColor.swift
+//  Surf-iOS(Artem-Galay)
+//
+//  Created by Артем Галай on 3.02.23.
+//
+
+import UIKit
+
+enum CommonColor {
+    // #DEE3EB
+    static let lightGray = UIColor(hex: "#DEE3EB")
+    // #2F5770
+    static let greenBlue = UIColor(hex: "#2F5770")
+}
